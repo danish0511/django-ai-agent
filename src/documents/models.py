689ecf63aa388c7ddb48b7d65a11b00ae14ae0c5
danish0511 <dsdd0511@gmail.com>
2025-06-11 16:29:38 +0530

    designing database table in django

---
+++ src/documents/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.conf import settings
+
+# Create your models here.
+
+User = settings.AUTH_USER_MODEL
+
+class Document(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(default="Title")
+    content = models.TextField(blank=True, null=True)
+    active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
