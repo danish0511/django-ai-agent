40c3ef7d04739ffd8bf793f935ebd8f9eb5f8323
danish0511 <dsdd0511@gmail.com>
2025-06-11 18:45:59 +0530

    django users and admin

--- src/documents/models.py
+++ src/documents/models.py
@@ -11,14 +11,13 @@ class Document(models.Model):
     title = models.CharField(default="Title")
     content = models.TextField(blank=True, null=True)
     active = models.BooleanField(default=True)
-    active_at = models.DateTimeField(auto_now_add=False, auto_now=False, null=True)
+    active_at = models.DateTimeField(auto_now_add=False, auto_now=False, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    updated_at  = models.DateTimeField(auto_now=True)
 
-def save(self, *args, **kwargs):
-    if self.active and self.active_At is None:
-        self.active_at = timezone.now()
-    else:
-        self.active_at = None
-    
-    super().save(*args, **kwargs)
+    def save(self, *args, **kwargs):
+        if self.active and self.active_at is None:
+            self.active_at = timezone.now()
+        else:
+            self.active_at = None
+        super().save(*args, **kwargs)
