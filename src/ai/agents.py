f14f4e23acb41c44ed5ab86c6f604aa11ba45a0e
danish0511 <dsdd0511@gmail.com>
2025-06-15 22:45:03 +0530

    adding memory to agent

--- src/ai/agents.py
+++ src/ai/agents.py
@@ -3,12 +3,13 @@ from ai.llms import get_gemini_model
 from ai.tools import document_tools
 
 
-def get_document_agent():
+def get_document_agent(checkpointer=None):
     model = get_gemini_model()
     
     agent = create_react_agent(
         model=model,  
         tools=document_tools,
-        prompt="You are a helpful assistant in managing a user's documents within this app"  
+        prompt="You are a helpful assistant in managing a user's documents within this app",
+        checkpointer=checkpointer
     )
     return agent
